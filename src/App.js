2ef2de66dc8690d512132b5d633d4f9d340b9d0b
Tony Eneh <aeneh90@gmail.com>
2020-05-09 14:07:29 +0100

    Implement routing to cars and filters pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import './App.css';
 import FiltersList from './Components/FiltersList/FiltersList';
+import CarsList from './Components/CarsList/CarsList';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+
 
 function App(){
   return (
     <div className="App">
-      <FiltersList />
+      <Router>
+        <Switch>
+          <Route path='/cars'>
+            <CarsList />
+          </Route>
+          <Route path='/'>
+            <FiltersList />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
