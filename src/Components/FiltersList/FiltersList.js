641f10efe7fd6ab814d1fda1de68eeb0f0526b9a
Tony Eneh <aeneh90@gmail.com>
2020-05-14 13:29:49 +0100

    Fetch filters from api
    Try fetching filters from provided api endpoint.
    If it fails, then use filters locally stored in filters.js file

--- src/Components/FiltersList/FiltersList.js
+++ src/Components/FiltersList/FiltersList.js
@@ -5,20 +5,47 @@ import { filters } from '../../filter';
 import { Link } from 'react-router-dom';
 
 class FiltersList extends React.Component {
-  render() {
-    if (!filters) {
-      return <div>Error: could not load the cars filters}</div>;
-    } else if (!filters.length) {
-      return <div>You have no filters at the moment.</div>;
-    } else {
-      return (
-        <div className="filtersList">
-          {filters.map((item, index) => (
-              <FilterItem key={item.id} item={item} />
-          ))}
-        </div>
-      );
+  constructor(props){
+    super(props);
+    this.state = {
+      filters: [],
+      isLoading: true,
+      error: false
     }
+  }
+
+  componentDidMount(){
+    console.log(filters);
+    fetch('https://ven10.co/assessment/filter.json')
+    .then(res => res.json())
+    .then(data => this.setState({filters: data, isLoading: false}))
+    .catch(err => {
+      this.setState({error: true, isLoading: false}); 
+      console.error(err)
+    })
+    .finally(() => {
+      // if it doesn't fetch, use local source
+      console.log('finally ran')
+      console.log(this.state.filters)
+      if(!this.state.filters.length){
+        this.setState({filters: filters, error: false, isLoading: false})
+      }
+      console.log('filters after finally', this.state.filters)
+    });
+  };
+
+  render() {
+    return (
+      <div className="filtersList">
+        {this.state.isLoading &&  <div>Loading...</div>}
+        {this.state.error && <div>Error: could not load the cars. Try some seconds later</div>}
+        {this.state.filters.length &&
+          filters.map((item, index) => 
+              <FilterItem key={item.id} item={item} />
+          )}
+        }
+      </div>
+    );
   }
 }
 
